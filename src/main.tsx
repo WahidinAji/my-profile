974a72b82aa508a91c01090f24e899711e471d3c
WahidinAji <a17wahidin@gmail.com>
2025-06-17 19:47:03 +0700

    fix missing context

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { QueryClient } from '@tanstack/react-query'
 // import App from './App.tsx'
 import './index.css'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
+// Create a QueryClient instance
+const queryClient = new QueryClient()
+
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ 
+    routeTree,
+    context: {
+        queryClient
+    }
+})
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
